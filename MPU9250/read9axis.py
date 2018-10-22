44dc74bf6a0a4a50b97733a4cefc8af08adb3ffc
baribalazs <30393879+baribalazs@users.noreply.github.com>
2018-10-22 23:13:03 +0200

    Update read9axis.py

--- MPU9250/read9axis.py
+++ MPU9250/read9axis.py
@@ -1,40 +1,43 @@
 # coding: utf-8
 ## @package faboMPU9250
 #  This is a library for the FaBo 9AXIS I2C Brick.
-#
-#  http://fabo.io/202.html
-#
-#  Released under APACHE LICENSE, VERSION 2.0
-#
-#  http://www.apache.org/licenses/
-#
-#  FaBo <info@fabo.io>
 
-import FaBo9Axis_MPU9250
+
+import MPU9250
 import time
 import sys
+import csv
 
-mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+mpu9250 = MPU9250.MPU9250()
+
 
 try:
-    while True:
-        accel = mpu9250.readAccel()
-        print " ax = " , ( accel['x'] )
-        print " ay = " , ( accel['y'] )
-        print " az = " , ( accel['z'] )
+    with open('data1.csv', 'w') as f:
+        datasave = csv.writer(f)
+    
+        while True:
+            accel = mpu9250.readAccel()
+            print(" ax = " , ( accel['x'] ))
+            print(" ay = " , ( accel['y'] ))
+            print(" az = " , ( accel['z'] ))
 
-        gyro = mpu9250.readGyro()
-        print " gx = " , ( gyro['x'] )
-        print " gy = " , ( gyro['y'] )
-        print " gz = " , ( gyro['z'] )
+            gyro = mpu9250.readGyro()
+            print(" gx = " , ( gyro['x'] ))
+            print(" gy = " , ( gyro['y'] ))
+            print(" gz = " , ( gyro['z'] ))
 
-        #mag = mpu9250.readMagnet()
-        #print " mx = " , ( mag['x'] )
-        #print " my = " , ( mag['y'] )
-        #print " mz = " , ( mag['z'] )
-        #print
+            mag = mpu9250.readMagnet()
+            print(" mx = " , ( mag['x'] ))
+            print(" my = " , ( mag['y'] ))
+            print(" mz = " , ( mag['z'] ))
 
-        time.sleep(0.5)
+            temper = mpu9250.readTemperature()
+            print("temp", (temper['temp']))
+            print()
+
+            datasave.writerow([accel['x'], accel['y'], accel['z']])
+
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
     sys.exit()
