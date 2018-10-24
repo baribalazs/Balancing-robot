7fcab9852755ea1a831401aa5c3d5d9436439ba7
baribalazs <30393879+baribalazs@users.noreply.github.com>
2018-10-24 17:36:50 +0200

    Add files via upload

---
+++ MPU9250/Kalman.py
@@ -0,0 +1,187 @@
+#Kalman filter implementation for MPU9250
+
+import MPU9250
+import time
+import math
+import sys
+import csv
+import datetime
+import os
+
+RadToDeg = 57.29578                 #Radian to degree
+H_Pi = 3.14159265358979323846       #PI value to carry
+GyroGain = 0.015267                 #[LSB/dps] catalogue data
+AA = 0.40                           #Complementary filter constant
+
+#Magnetic sensor calibration values
+magXmin =  0
+magYmin =  0
+magZmin =  0
+magXmax =  0
+magYmax =  0
+magZmax =  0
+
+#Kalman filter parameters
+Q_angle = 0.02
+Q_gyro = 0.0015
+R_angle = 0.005
+y_bias = 0.0
+x_bias = 0.0
+XP_00 = 0.0
+XP_01 = 0.0
+XP_10 = 0.0
+XP_11 = 0.0
+YP_00 = 0.0
+YP_01 = 0.0
+YP_10 = 0.0
+YP_11 = 0.0
+KFangleX = 0.0
+KFangleY = 0.0
+
+def kalmanFilterY ( accAngle, gyroRate, DT):
+	y=0.0
+	S=0.0
+
+	global KFangleY
+	global Q_angle
+	global Q_gyro
+	global y_bias
+	global YP_00
+	global YP_01
+	global YP_10
+	global YP_11
+
+	KFangleY = KFangleY + DT * (gyroRate - y_bias)
+
+	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
+	YP_01 = YP_01 + ( - DT * YP_11 )
+	YP_10 = YP_10 + ( - DT * YP_11 )
+	YP_11 = YP_11 + ( + Q_gyro * DT )
+
+	y = accAngle - KFangleY
+	S = YP_00 + R_angle
+	K_0 = YP_00 / S
+	K_1 = YP_10 / S
+	
+	KFangleY = KFangleY + ( K_0 * y )
+	y_bias = y_bias + ( K_1 * y )
+	
+	YP_00 = YP_00 - ( K_0 * YP_00 )
+	YP_01 = YP_01 - ( K_0 * YP_01 )
+	YP_10 = YP_10 - ( K_1 * YP_00 )
+	YP_11 = YP_11 - ( K_1 * YP_01 )
+	
+	return KFangleY
+
+def kalmanFilterX ( accAngle, gyroRate, DT):
+	x=0.0
+	S=0.0
+
+	global KFangleX
+	global Q_angle
+	global Q_gyro
+	global x_bias
+	global XP_00
+	global XP_01
+	global XP_10
+	global XP_11
+
+
+	KFangleX = KFangleX + DT * (gyroRate - x_bias)
+
+	XP_00 = XP_00 + ( - DT * (XP_10 + XP_01) + Q_angle * DT )
+	XP_01 = XP_01 + ( - DT * XP_11 )
+	XP_10 = XP_10 + ( - DT * XP_11 )
+	XP_11 = XP_11 + ( + Q_gyro * DT )
+
+	x = accAngle - KFangleX
+	S = XP_00 + R_angle
+	K_0 = XP_00 / S
+	K_1 = XP_10 / S
+	
+	KFangleX = KFangleX + ( K_0 * x )
+	x_bias = x_bias + ( K_1 * x )
+	
+	XP_00 = XP_00 - ( K_0 * XP_00 )
+	XP_01 = XP_01 - ( K_0 * XP_01 )
+	XP_10 = XP_10 - ( K_1 * XP_00 )
+	XP_11 = XP_11 - ( K_1 * XP_01 )
+	
+	return KFangleX
+
+ac = mpu9250.readAccel()        #accelerometer
+gy = mpu9250.readGyro()         #gyro
+ma = mpu9250.readMagnet()       #magneto sensor
+te = mpu9250.readTemperature()  #temperature
+
+gyroXangle = 0.0
+gyroYangle = 0.0
+gyroZangle = 0.0
+CFangleX = 0.0
+CFangleY = 0.0
+kalmanX = 0.0
+kalmanY = 0.0
+
+a = datetime.datetime.now()     #time at beginning
+
+while True:
+
+
+    #the incoming data
+    ACCx = ac['x']
+    ACCy = ac['y']
+    ACCz = ac['z']
+    GYRx = gy['x']
+    GYRy = gy['y']
+    GYRz = gy['z']
+    MAGx = ma['x']
+    MAGy = gy['y']
+    MAGz = gy['z']
+
+    #Apply compass calibration    
+    MAGx -= (magXmin + magXmax) /2 
+    MAGy -= (magYmin + magYmax) /2 
+    MAGz -= (magZmin + magZmax) /2
+
+    ##Calculate loop Period(LP). How long between Gyro Reads
+    b = datetime.datetime.now() - a
+    a = datetime.datetime.now()
+    LP = b.microseconds/(1000000*1.0)
+    
+    #Convert Gyro raw to degrees per second - dps
+    rate_gyr_x =  GYRx * GyroGain
+    rate_gyr_y =  GYRy * GyroGain
+    rate_gyr_z =  GYRz * GyroGain
+
+    #Calculate the angles from the gyro. dps*sec=degree
+    gyroXangle+=rate_gyr_x*LP
+    gyroYangle+=rate_gyr_y*LP
+    gyroZangle+=rate_gyr_z*LP
+
+    #Convert accelerometer to angle/degree - careful at setup
+    AccXangle =  (math.atan2(-ACCy,-ACCz)*RadToDeg)
+    AccYangle =  (math.atan2(-ACCz,-ACCx)+M_PI)*RadToDeg
+
+    #Change the rotation value of the accelerometer to -/+ 180 and
+    #move the Y axis '0' point to up.  This makes it easier to read.
+    if AccYangle > 90:
+        AccYangle -= 270.0
+    else:
+        AccYangle += 90.0
+    
+    #Complementary filter used to combine the accelerometer and gyro values.
+    CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
+    CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
+
+    #Kalman filter used to combine the accelerometer and gyro values.
+    kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
+    kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
+
+    #Normalise acc values
+    accXnorm = ACCx/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+    accYnorm = ACCy/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
+
+    pitch = math.asin(accXnorm)
+    roll = -math.asin(accYnorm/math.cos(pitch))
+
+    
--- MPU9250/read9axis.py
+++ MPU9250/read9axis.py
@@ -1,7 +1,5 @@
-# coding: utf-8
-## @package faboMPU9250
-#  This is a library for the FaBo 9AXIS I2C Brick.
-
+# Created to test and evaluate complementary and Kalman Filter
+# allways invoke the registry writer first.
 
 import MPU9250
 import time
@@ -10,6 +8,44 @@ import csv
 
 mpu9250 = MPU9250.MPU9250()
 
+K = 0.98
+K1 = 1-K
+
+time_diff = 0.02
+
+def distance(a, b):
+    return math.sqrt((a*a) + (b*b))
+
+def y_rotation(x, y, z):
+    radians = math.atan2(x, distance(y, z))
+    return -math.degrees(radians)
+
+def x_rotation(x, y, z):
+    radians = math.atan2(y, distance(x, z))
+    return math.degrees(radians)
+
+accel = mpu9250.readAccel()
+gyro = mpu9250.readGyro()
+mag = mpu9250.readMagnet()
+temper = mpu9250.readTemperature()
+
+aTempX = accel['x']
+aTempY = accel['y']
+aTempZ = accel['z']
+
+gTempX = gyro['x']
+gTempY = gyro['y']
+gTempZ = gyro['z']
+
+last_x = x_rotation(aTempX, aTempY, aTempZ)
+last_y = y_rotation(aTempX, aTempY, aTempZ)
+
+gyro_offset_x = gTempX
+gyro_offset_y = gTempY
+
+gyro_total_x = (last_x) - gyro_offset_x
+gyro_total_y = (last_y) - gyro_offset_y
+    
 
 try:
     with open('data1.csv', 'w') as f:
@@ -17,27 +53,39 @@ try:
     
         while True:
             accel = mpu9250.readAccel()
-            print(" ax = " , ( accel['x'] ))
-            print(" ay = " , ( accel['y'] ))
-            print(" az = " , ( accel['z'] ))
-
             gyro = mpu9250.readGyro()
-            print(" gx = " , ( gyro['x'] ))
-            print(" gy = " , ( gyro['y'] ))
-            print(" gz = " , ( gyro['z'] ))
-
             mag = mpu9250.readMagnet()
-            print(" mx = " , ( mag['x'] ))
-            print(" my = " , ( mag['y'] ))
-            print(" mz = " , ( mag['z'] ))
-
             temper = mpu9250.readTemperature()
-            print("temp", (temper['temp']))
-            print()
+            
+            accelX = accel['x']
+            accelY = accel['y']
+            accelZ = accel['z']
 
-            datasave.writerow([accel['x'], accel['y'], accel['z']])
+            gyroX = gyro['x']
+            gyroY = gyro['y']
+            gyroZ = gyro['z']
 
-        time.sleep(0.05)
+            gyroX -= gyro_offset_x
+            gyroY -= gyro_offset_y
+
+            gyro_x_delta = (gyroX * time_diff)
+            gyro_y_delta = (gyroY * time_diff)
+
+            gyro_total_x += gyro_x_delta
+            gyro_total_y += gyro_y_delta
+
+            rotation_x = x_rotation(accelX, accelY, accelZ)
+            rotation_y = y_rotation(accelX, accelY, accelZ)
+    
+            #Complementary Filter
+            last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
+            last_y = K * (last_y + gyro_y_delta) + (K1 * rotation_y)
+
+            #print("temp", (temper['temp']))
+            
+            datasave.writerow([accelX, accelY, accelZ, gyroX, gyroY, gyroZ last_x, last_y, mag['x'], mag['y'], mag['z']])
+
+        time.sleep(0.02)
 
 except KeyboardInterrupt:
     sys.exit()
